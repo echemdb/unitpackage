1711d0e2bc3bffbae9826f374b45ce4456f3311b
Julian Rüth <julian.rueth@fsfe.org>
2022-05-20 00:15:12 +0200

    bumped version to 0.3.0

--- doc/conf.py
+++ doc/conf.py
@@ -2,7 +2,7 @@ project = 'echemdb'
 copyright = '2022, the echemdb authors'
 author = 'the echemdb authors'
 
-release = '0.2.0'
+release = '0.3.0'
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo", "myst_nb"]
 
