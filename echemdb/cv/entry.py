d5d8b5e7c93c414a086dd83e3689a943f9fad114
Albert Engstfeld <albert.engstfeld@gmail.com>
2023-02-14 16:56:42 +0100

    fix doctest

--- echemdb/cv/entry.py
+++ echemdb/cv/entry.py
@@ -101,7 +101,7 @@ class Entry:
             >>> dir(entry) # doctest: +NORMALIZE_WHITESPACE
             ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__',
             '__eq__', '__format__', '__ge__', '__getattr__', '__getattribute__',
-            '__getitem__', '__gt__', '__hash__', '__init__', '__init_subclass__',
+            '__getitem__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__',
             '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__',
             '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__',
             '__subclasshook__', '__weakref__',
