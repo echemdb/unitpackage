e43c4a5e387e04f4e5848518c37e7c48aeeaa91e
Albert Engstfeld <albert.engstfeld@gmail.com>
2023-02-21 12:13:26 +0100

    Update frictionless dependency

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=[
       "astropy>=5,<6",
       "filelock>=3,<4",
-      "frictionless>=4.29,<5",
+      "frictionless>=5.5.0,<6",
       "matplotlib>=3.5.0,<4",
       "pandas>=1,<2",
       "plotly>=5,<6",
