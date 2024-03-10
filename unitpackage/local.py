9b857ae1ab546ae71889b6847532f9ec8b8b480e
Albert Engstfeld <albert.engstfeld@gmail.com>
2024-03-10 12:42:21 +0100

    Add `entry.from_csv`

--- unitpackage/local.py
+++ unitpackage/local.py
@@ -30,6 +30,36 @@ from glob import glob
 import pandas as pd
 from frictionless import Package, Resource, Schema
 
+def create_df_resource(package, resource_name='echemdb'):
+    if not package.resources:
+        raise ValueError("dataframe resource can not be created since package has no resources")
+    descriptor_path = (
+        package.resources[0].basepath + "/" + package.resources[0].path
+    )
+    df = pd.read_csv(descriptor_path)
+    df_resource = Resource(df)
+    df_resource.infer()
+    df_resource.name = resource_name
+    return df_resource
+
+def collect_datapackage(filename):
+    package = Package(filename)
+    # if not package.resources:
+    #     raise ValueError(f"package {filename} has no CSV resources")
+    # descriptor_path = (
+    #     package.resources[0].basepath + "/" + package.resources[0].path
+    # )
+    # df = pd.read_csv(descriptor_path)
+    # df_resource = Resource(df)
+    # df_resource.infer()
+    # df_resource.name = "echemdb"
+    package.add_resource(create_df_resource(package))
+    package.get_resource("echemdb").schema = Schema.from_descriptor(
+        package.resources[0].schema.to_dict()
+    )
+
+    return package
+
 def collect_datapackages(data):
     r"""
     Return a list of data packages defined in the directory `data` and its
@@ -48,32 +78,35 @@ def collect_datapackages(data):
 
     descriptors = glob(os.path.join(data, "**", "*.json"), recursive=True)
 
-    # Read the package descriptors and append the data as pandas dataframe in a new resource
+
 
     packages = []
 
     for descriptor in descriptors:
-        package = Package(descriptor)
+        # package = Package(descriptor)
 
-        if not package.resources:
-            raise ValueError(f"package {descriptor} has no CSV resources")
-        descriptor_path = (
-            package.resources[0].basepath + "/" + package.resources[0].path
-        )
-        df = pd.read_csv(descriptor_path)
-        df_resource = Resource(df)
-        df_resource.infer()
-        df_resource.name = "echemdb"
-        package.add_resource(df_resource)
-        package.get_resource("echemdb").schema = Schema.from_descriptor(
-            package.resources[0].schema.to_dict()
-        )
+        # if not package.resources:
+        #     raise ValueError(f"package {descriptor} has no CSV resources")
+        # descriptor_path = (
+        #     package.resources[0].basepath + "/" + package.resources[0].path
+        # )
+        # df = pd.read_csv(descriptor_path)
+        # df_resource = Resource(df)
+        # df_resource.infer()
+        # df_resource.name = "echemdb"
+        # package.add_resource(df_resource)
+        # package.get_resource("echemdb").schema = Schema.from_descriptor(
+        #     package.resources[0].schema.to_dict()
+        # )
 
-        packages.append(package)
+        # packages.append(package)
+        #--------------------------------------------
+        # Read the package descriptors and append the data as pandas dataframe in a new resource
+        packages.append(collect_datapackage(descriptor))
 
     return packages
 
-def create_unitpackage(csvname, outdir, metadata=None, fields={}, validate_astropy=True):
+def create_unitpackage(csvname, metadata=None, fields=[]):
     r"""
     Return a data package built from a :param:`metadata` dict and tabular data
     in :param:`csvname` str.
@@ -88,7 +121,8 @@ def create_unitpackage(csvname, outdir, metadata=None, fields={}, validate_astro
         resources=[
             Resource(
                 path=csv_basename,
-                basepath=outdir or os.path.dirname(csvname),
+                # basepath=outdir or os.path.dirname(csvname),
+                basepath= os.path.dirname(csvname),
             )
         ],
     )
@@ -138,80 +172,80 @@ def create_unitpackage(csvname, outdir, metadata=None, fields={}, validate_astro
 
     resource.schema = Schema.from_descriptor({"fields": new_fields})
 
-    with open(
-        _outfile(csv_basename, suffix=".package.json", outdir=outdir),
-        mode="w",
-        encoding="utf-8",
-    ) as json:
-        _write_metadata(json, package.to_dict())
+    # with open(
+    #     _outfile(csv_basename, suffix=".package.json", outdir=outdir),
+    #     mode="w",
+    #     encoding="utf-8",
+    # ) as json:
+    #     _write_metadata(json, package.to_dict())
 
     return package
 
 # from svgdigitizer
-def _outfile(template, suffix=None, outdir=None):
-    r"""
-    Return a file name for writing.
+# def _outfile(template, suffix=None, outdir=None):
+#     r"""
+#     Return a file name for writing.
 
-    The file is named like `template` but with the suffix changed to `suffix`
-    if specified. The file is created in `outdir`, if specified, otherwise in
-    the directory of `template`.
+#     The file is named like `template` but with the suffix changed to `suffix`
+#     if specified. The file is created in `outdir`, if specified, otherwise in
+#     the directory of `template`.
 
-    EXAMPLES::
+#     EXAMPLES::
 
-        >>> from svgdigitizer.test.cli import invoke, TemporaryData
-        >>> with TemporaryData("**/xy.svg") as directory:
-        ...     outname = _outfile(os.path.join(directory, "xy.svg"), suffix=".csv")
-        ...     with open(outname, mode="wb") as csv:
-        ...         _ = csv.write(b"...")
-        ...     os.path.exists(os.path.join(directory, "xy.csv"))
-        True
+#         >>> from svgdigitizer.test.cli import invoke, TemporaryData
+#         >>> with TemporaryData("**/xy.svg") as directory:
+#         ...     outname = _outfile(os.path.join(directory, "xy.svg"), suffix=".csv")
+#         ...     with open(outname, mode="wb") as csv:
+#         ...         _ = csv.write(b"...")
+#         ...     os.path.exists(os.path.join(directory, "xy.csv"))
+#         True
 
-    ::
+#     ::
 
-        >>> with TemporaryData("**/xy.svg") as directory:
-        ...     outname = _outfile(os.path.join(directory, "xy.svg"), suffix=".csv", outdir=os.path.join(directory, "subdirectory"))
-        ...     with open(outname, mode="wb") as csv:
-        ...         _ = csv.write(b"...")
-        ...     os.path.exists(os.path.join(directory, "subdirectory", "xy.csv"))
-        True
+#         >>> with TemporaryData("**/xy.svg") as directory:
+#         ...     outname = _outfile(os.path.join(directory, "xy.svg"), suffix=".csv", outdir=os.path.join(directory, "subdirectory"))
+#         ...     with open(outname, mode="wb") as csv:
+#         ...         _ = csv.write(b"...")
+#         ...     os.path.exists(os.path.join(directory, "subdirectory", "xy.csv"))
+#         True
 
-    """
-    if suffix is not None:
-        template = f"{os.path.splitext(template)[0]}{suffix}"
+#     """
+#     if suffix is not None:
+#         template = f"{os.path.splitext(template)[0]}{suffix}"
 
-    if outdir is not None:
-        template = os.path.join(outdir, os.path.basename(template))
+#     if outdir is not None:
+#         template = os.path.join(outdir, os.path.basename(template))
 
-    os.makedirs(os.path.dirname(template) or ".", exist_ok=True)
+#     os.makedirs(os.path.dirname(template) or ".", exist_ok=True)
 
-    return template
+#     return template
 
-# from svgdigitizer
-def _write_metadata(out, metadata):
-    r"""
-    Write `metadata` to the `out` stream in JSON format.
+# # from svgdigitizer
+# def _write_metadata(out, metadata):
+#     r"""
+#     Write `metadata` to the `out` stream in JSON format.
 
-    This is a helper method for :meth:`_create_outfiles`.
-    """
+#     This is a helper method for :meth:`_create_outfiles`.
+#     """
 
-    def defaultconverter(item):
-        r"""
-        Return `item` that Python's json package does not know how to serialize
-        in a format that Python's json package does know how to serialize.
-        """
-        from datetime import date, datetime
+#     def defaultconverter(item):
+#         r"""
+#         Return `item` that Python's json package does not know how to serialize
+#         in a format that Python's json package does know how to serialize.
+#         """
+#         from datetime import date, datetime
 
-        # The YAML standard knows about dates and times, so we might see these
-        # in the metadata. However, standard JSON does not know about these so
-        # we need to serialize them as strings explicitly.
-        if isinstance(item, (datetime, date)):
-            return str(item)
+#         # The YAML standard knows about dates and times, so we might see these
+#         # in the metadata. However, standard JSON does not know about these so
+#         # we need to serialize them as strings explicitly.
+#         if isinstance(item, (datetime, date)):
+#             return str(item)
 
-        raise TypeError(f"Cannot serialize ${item} of type ${type(item)} to JSON.")
+#         raise TypeError(f"Cannot serialize ${item} of type ${type(item)} to JSON.")
 
-    import json
+#     import json
 
-    json.dump(metadata, out, default=defaultconverter, ensure_ascii=False, indent=4)
-    # json.dump does not save files with a newline, which compromises the tests
-    # where the output files are compared to an expected json.
-    out.write("\n")
+#     json.dump(metadata, out, default=defaultconverter, ensure_ascii=False, indent=4)
+#     # json.dump does not save files with a newline, which compromises the tests
+#     # where the output files are compared to an expected json.
+#     out.write("\n")
