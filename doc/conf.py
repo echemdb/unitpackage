68c96bba64ed3baf6b82033adcf6909b3a1666f8
Albert Engstfeld <albert.engstfeld@gmail.com>
2025-08-05 14:34:46 +0200

    bumped version to 0.10.0

--- doc/conf.py
+++ doc/conf.py
@@ -2,7 +2,7 @@ project = 'unitpackage'
 copyright = '2022-2023, the unitpackage authors'
 author = 'the unitpackage authors'
 
-release = '0.9.2'
+release = '0.10.0'
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo", "myst_nb"]
 
