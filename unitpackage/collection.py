ee6b370091ddcbfae2331417203b46c53d2d2d0c
Albert Engstfeld <albert.engstfeld@gmail.com>
2025-08-18 13:27:16 +0200

    Improve documentation

--- unitpackage/collection.py
+++ unitpackage/collection.py
@@ -640,7 +640,10 @@ class Collection:
 
     @property
     def identifiers(self):
-        """Return a list of identifiers.
+        """Return a list of identifiers of the collection,
+        i.e., the names of the resources in the datapackage.
+
+        This method is basically equivalent to `package.resource_names`.
 
         EXAMPLES::
 
